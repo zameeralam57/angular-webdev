8444875881ab30de6c1de77b025531e29f760e4f
unknown <zameeralam57@gmail.com>
2024-10-10 01:03:17 +0530

    add file

--- angular-webdev/src/app/app.component.ts
+++ angular-webdev/src/app/app.component.ts
@@ -1,18 +1,22 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet, Routes } from '@angular/router';
 //import { NgModule } from '@angular/core';
 //import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms'; //
+
 @Component({
-  selector: 'app-root',
+  selector: 'app-root', 
   standalone: true,
-  imports: [RouterOutlet ,  FormsModule],
+  imports: [RouterOutlet ,  FormsModule, RouterLink, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css' 
   
 })
 
+
 export class AppComponent { 
   title = 'my-app';
   Name: string = ''; // Define a property to bind with ngModel 
 }
+
+
--- angular-webdev/src/app/app.routes.ts
+++ angular-webdev/src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { CourseComponent } from './course/course.component';
+import { ContactsComponent } from './contacts/contacts.component';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+    { path: 'app-course', component: CourseComponent },
+    {path: 'app-contacts', component: ContactsComponent}
+];
+
